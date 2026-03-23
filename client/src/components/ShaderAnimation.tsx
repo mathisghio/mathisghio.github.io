53352f0305e6335fe88e913e361f8caab322e8a2
Mathis Ghio <mghio1@insa-lyon.fr>
2026-03-23 02:06:30 +0100

    Update ShaderAnimation.tsx

--- client/src/components/ShaderAnimation.tsx
+++ client/src/components/ShaderAnimation.tsx
@@ -11,24 +11,27 @@ export function ShaderAnimation() {
     const container = containerRef.current
     const vertexShader = `void main() { gl_Position = vec4( position, 1.0 ); }`
     const fragmentShader = `
-      #define TWO_PI 6.2831853072
-      #define PI 3.14159265359
-      precision highp float;
-      uniform vec2 resolution;
-      uniform float time;
-      void main(void) {
-        vec2 uv = (gl_FragCoord.xy * 2.0 - resolution.xy) / min(resolution.x, resolution.y);
-        float t = time*0.05;
-        float lineWidth = 0.002;
-        vec3 color = vec3(0.0);
-        for(int j = 0; j < 3; j++){
-          for(int i=0; i < 5; i++){
-            color[j] += lineWidth*float(i*i) / abs(fract(t - 0.01*float(j)+float(i)*0.01)*5.0 - length(uv) + mod(uv.x+uv.y, 0.2));
-          }
-        }
-        gl_FragColor = vec4(color[0],color[1],color[2],1.0);
+  #define TWO_PI 6.2831853072
+  #define PI 3.14159265359
+  precision highp float;
+  uniform vec2 resolution;
+  uniform float time;
+  float random(float x) {
+    return fract(sin(x) * 1e4);
+  }
+  void main(void) {
+    vec2 uv = (gl_FragCoord.xy * 2.0 - resolution.xy) / min(resolution.x, resolution.y);
+    float t = time*0.12+random(uv.x)*0.4;
+    float lineWidth = 0.003;
+    vec3 color = vec3(0.0);
+    for(int j = 0; j < 3; j++){
+      for(int i=0; i < 5; i++){
+        color[j] += lineWidth*float(i*i) / abs(fract(t - 0.01*float(j)+float(i)*0.01)*0.4 - length(uv));
       }
-    `
+    }
+    gl_FragColor = vec4(color[0],color[1],color[2],1.0);
+  }
+`
     const camera = new THREE.Camera()
     camera.position.z = 1
     const scene = new THREE.Scene()
