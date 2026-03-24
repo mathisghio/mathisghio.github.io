dc48e878eb3f80d4d05db21b0d7127903e588a50
Mathis Ghio <mghio1@insa-lyon.fr>
2026-03-24 11:55:31 +0100

    Update GoatSection.tsx

--- client/src/components/GoatSection.tsx
+++ client/src/components/GoatSection.tsx
@@ -1,17 +1,7 @@
 import { useEffect, useRef, useState } from 'react'
+import { useInView } from '@/hooks/useInView'
 
 const PODIUM_IMG = 'https://d2xsxph8kpxj0f.cloudfront.net/310519663455151996/gejRxzH8i7adBy6yjBjf3b/podium-main_ca0aec18.JPG'
-
-function useInView(threshold = 0.1) {
-  const ref = useRef<HTMLDivElement>(null)
-  const [inView, setInView] = useState(false)
-  useEffect(() => {
-    const obs = new IntersectionObserver(([entry]) => { if (entry.isIntersecting) setInView(true) }, { threshold })
-    if (ref.current) obs.observe(ref.current)
-    return () => obs.disconnect()
-  }, [threshold])
-  return { ref, inView }
-}
 
 export function GoatSection() {
   const { ref, inView } = useInView(0.1)
