6b1914e14ebf0c9e057771978d9457ed6fd5a49d
Mathis Ghio <mghio1@insa-lyon.fr>
2026-03-28 09:55:41 +0100

    Create GalleryRainBackground.tsx

---
+++ client/src/components/GalleryRainBackground.tsx
@@ -0,0 +1,10 @@
+import './GalleryRainBackground.css'
+
+export function GalleryRainBackground() {
+  return (
+    <>
+      <div className="grb-rain" aria-hidden />
+      <div className="grb-overlay" aria-hidden />
+    </>
+  )
+}
