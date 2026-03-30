fa20a7cdf9bd7a0a05ef2bbfca6425ad768cfaf6
Mathis Ghio <mghio1@insa-lyon.fr>
2026-03-30 08:07:02 +0200

    Update HeroSection.tsx

--- client/src/components/HeroSection.tsx
+++ client/src/components/HeroSection.tsx
@@ -50,7 +50,7 @@ export function HeroSection() {
       </div>
 
       {/* ── Contenu hero ── */}
-      <div className="relative z-40 h-full flex flex-col pb-20 lg:pb-28">
+      <div className="relative z-40 h-full flex flex-col pb-32 lg:pb-40">
         {/* Spacer rigide = hauteur du nav. Garantit que le contenu ne remonte JAMAIS derrière lui */}
         <div style={{ minHeight: '90px', flexShrink: 0 }} />
         {/* marginTop: auto colle le contenu en bas */}
